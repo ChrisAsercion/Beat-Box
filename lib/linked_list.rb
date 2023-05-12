e4c0560b4d6812f67d63341d82f26ee4bb0b474f
Chris Asercion <chris.asercion@gmail.com>
2023-05-11 18:30:02 -0600

    initial commit

---
+++ lib/linked_list.rb
@@ -0,0 +1,7 @@
+class LinkedList
+    attr_reader :head
+
+    def initialize
+        self.head = nil
+    end
+end
---
+++ spec/linked_list_spec.rb
@@ -0,0 +1,6 @@
+require "./lib/linked_list"
+require "./lib/node"
+
+it "linked list exists" do
+    list = LinkedList.new
+end
